8119f744b0ba725b95272cfa3ea2ea09b70006f6
Sushma vasamsetti <121925007+vasamsettisushma@users.noreply.github.com>
2023-10-08 10:25:55 +0530

    Update server.js

--- mail/src/server.js
+++ mail/src/server.js
@@ -3,12 +3,8 @@ const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const app = express();
-const port = 3000; // You can use any port you prefer
-
-// Middleware to parse JSON data
+const port = 3000; 
 app.use(bodyParser.json());
-
-// Configure nodemailer with your email service provider's SMTP settings
 
 const transporter = nodemailer.createTransport({
   service: 'Gmail',
@@ -19,8 +15,6 @@ const transporter = nodemailer.createTransport({
 });
 
 
-
-// Route to handle form submission
 app.post('/send-email', (req, res) => {
   const { name, email, message } = req.body;
 
@@ -31,7 +25,6 @@ app.post('/send-email', (req, res) => {
     text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
   };
 
-  // Send email
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error(error);
